b1239c370d27424df67c125fe23b2f6a9b67583f
triplem <160079+triplem@users.noreply.github.com>
2024-12-29 20:13:01 +0100

    add documentation, add some aliasses (thnx to Distrotube)

--- dot_doom.d/utility.el
+++ dot_doom.d/utility.el
@@ -59,3 +59,18 @@
 
 ;; store value on disk if emacs closes
 (add-hook 'kill-emacs-hook 'triplem/save-input-history)
+
+
+;; Close the current buffer and delete the file
+;; based on http://emacsredux.com/blog/2013/04/03/delete-file-and-buffer/
+(defun delete-file-and-buffer ()
+  "Kill the current buffer and deletes the file it is visiting."
+  (interactive)
+  (let ((filename (buffer-file-name)))
+    (if filename
+        (if (y-or-n-p (concat "Do you really want to delete file " filename " ?"))
+            (progn
+              (delete-file filename)
+              (message "Deleted file %s." filename)
+              (kill-buffer)))
+      (message "Not a file visiting buffer!"))))
